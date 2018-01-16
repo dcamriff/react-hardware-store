a09c2c2b0717db414cfad1df94ded74f99f1ef55
dcamriff <driffelatl@gmail.com>
2018-01-15 21:43:18 -0500

    made some additions to CartView and HomePage

--- src/components/CartView.js
+++ src/components/CartView.js
@@ -6,6 +6,23 @@ const CartView = (props) => {
     const productList = props.productList
 
     const totalPrice = productList.reduce((totalPrice, product) => {
+        return totalPrice + product.price
+    }, 0.00)
 
-    }
+    return (
+        <div className="small-view-box">
+            <h1>Cart</h1>
+
+            <ProductList
+                productList={productList}
+                viewMode={'CART'}
+                addProductToCart={props.addProductToCart}
+                removeProductFromCart={props.removeProductFromCart}/>
+            <div>
+                <h3>Total Price: {totalPrice}</h3>
+            </div>
+        </div>
+    )
 }
+
+export default CartView
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import AdminView from './AdminView'
 import ShopView from './ShopView'
+import CartView from './CartView'
 
 class HomePage extends Component {
 
@@ -9,7 +10,8 @@ class HomePage extends Component {
 
         this.state = {
             itemCurrentlyOnSale: 'A Hammer',
-            editSaleItem: true,
+            editSaleItem: false,
+            showAdminView: false,
             productList: [
                 {
                     productName: 'Hammer',
@@ -20,13 +22,19 @@ class HomePage extends Component {
                     description: 'Itsa nail',
                     price: 0.12
                 }
-            ]
+            ],
+            cartList: []
         }
     }
 
     toggleEditSaleItem = () => {
-        const editSaleItem = !this.state.editSaleItem;
+        const editSaleItem = !this.state.editSaleItem
         this.setState({editSaleItem})
+    }
+
+    toggleAdminView = () => {
+        const showAdminView = !this.state.showAdminView
+        this.setState({showAdminView})
     }
 
     handleItemCurrentlyOnSaleChange = (event) => {
@@ -34,8 +42,9 @@ class HomePage extends Component {
         this.setState({itemCurrentlyOnSale})
     }
 
-    _addNewProductToProductList = (newProduct) => {
+    addNewProductToProductList = (newProduct) => {
         const productList = [...this.state.productList]
+
         productList.push(newProduct)
         this.setState({productList})
     }
@@ -46,56 +55,100 @@ class HomePage extends Component {
         this.setState({productList})
     }
 
-    render() {
+    addProductToCart = (idToAddToCart) => {
+        const cartList = [...this.state.cartList]
+        const product = {
+            ...this.state.productList[idToAddToCart]
+        }
 
-        return (
+        cartList.push(product)
+
+        this.setState({cartList})
+    }
+
+removeProductFromCart = (idToRemove) => {
+    const cartList = [...this.state.cartList]
+
+    cartList.splice(idToRemove, 1)
+    this.setState({cartList})
+}
+
+render() {
+    const adminView = <AdminView
+        productList={this.state.productList}
+        addNewProductToProductList={this.addNewProductToProductList}
+        deleteProductFromProductList={this.deleteProductFromProductList}/>
+
+    const shopView = <ShopView
+        productList={this.state.productList}
+        addProductToCart={this.addProductToCart}/>
+
+    return (
+        <div>
             <div>
-
-                <h1>My Hardware Store</h1>
-                <div>
+                <div id="home-page-nav">
+                    <h1>My Hardware Store</h1>
                     <span>Currently On Sale: {this.state.itemCurrentlyOnSale}!</span>
-                    <span>
+
+                    <div>
+                        {this.state.editSaleItem
+                            ? <div>
+                                    <input
+                                        onChange={this.handleItemCurrentlyOnSaleChange}
+                                        value={this.state.itemCurrentlyOnSale}
+                                        type="text"/>
+                                </div>
+                            : null}
+                    </div>
+                    <div>
                         <button onClick={this.toggleEditSaleItem}>
                             {this.state.editSaleItem
                                 ? 'Hide'
                                 : 'Edit Sale Item'}
                         </button>
-                    </span>
-
-                    {this.state.editSaleItem
-                        ? <div>
-                                <input
-                                    onChange={this.handleItemCurrentlyOnSaleChange}
-                                    value={this.state.itemCurrentlyOnSale}
-                                    type="text"/>
-                            </div>
-                        : null
-}
-
-                    <AdminView
-                        productList={this.state.productList}
-                        addNewProductToProductList={this.addNewProductToProductList}/>
-
-                    {/* <ShopView /> */}
-
+                    </div>
+                    <div>
+                        <button onClick={this.toggleAdminView}>
+                            {this.state.showAdminView
+                                ? 'Show Shop View'
+                                : 'Show Admin View'}
+                        </button>
+                    </div>
                 </div>
             </div>
+
+            <div id="view-container">
+                {this.state.showAdminView ? adminView : shopView}
+
+                <CartView
+                    productList={this.state.cartList}
+                    removeProductFromCart={this.removeProductFromCart}/>
+            </div>
+        </div>
         )
     }
 }
+// //             /* {this.state.editSaleItem
+// //                     ? <div>
+// //                             <input
+// //                                 onChange={this.handleItemCurrentlyOnSaleChange}
+// //                                 value={this.state.itemCurrentlyOnSale}
+// //                                 type="text"/>
+// //                         </div>
+// //                     : null
+
+//         {/* <AdminView
+//                     productList={this.state.productList}
+//                     addNewProductToProductList={this.addNewProductToProductList}/> */}
 
 export default HomePage
-
-// You can not define a const in a class that's why you don't see const
-// toggleEditSaleItem. That's where we are here. So this is a custom method
-// below. It's telling us make a new item called SaleItem and then we are
-// bringing in really important method called this.setState which changes the
-// state. this.setState({editSaleItem}) is a pure function, meaning it is
-// stateless. Reads: Go take my state, and change it to editSaleItem. It is
-// deleting the old state and replacing with new state. When our state changes,
-// the page automatically updates. inside return statement, the
-// {this.state.editSaleItem...} is a ternary statement. The ? something : makes
-// it a ternary statement
-// Note: The input is a "controlled" input. We tell it what to do when it
-// changes. WE HAVE ORGANIZED THIS PROJECT SO THAT HOMEPAGE BECOMES TOP VIEW SO
-// THAT KINDA TAKES PLACE OF WHAT WE'D USE APP.JS FOR
+        
+        // You can not define a const in a class that's why you don't see const
+        // toggleEditSaleItem. That's where we are here. So this is a custom method
+        // below. It's telling us make a new item called SaleItem and then we are
+        // bringing in really important method called this.setState which changes the
+        // state. this.setState({editSaleItem}) is a pure function, meaning it is
+        // stateless. Reads: Go take my state, and change it to editSaleItem. It is
+        // deleting the old state and replacing with new state. When our state changes,
+        // the page automatically updates. inside return statement, the
+        // {this.state.editSaleItem ...}
